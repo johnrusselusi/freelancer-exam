3694aa8f5118f1598ec341ff2bd083671986102e
John Russel Usi <johnrusselusi@ymail.com>
2017-08-03 09:39:46 +0800

    Changed the title of the navigation bar into movie title.

--- myMovies/myMovies/MovieDetailViewController.swift
+++ myMovies/myMovies/MovieDetailViewController.swift
@@ -22,6 +22,7 @@ class MovieDetailViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        navigationItem.title = movie?.name
         setupViews()
     }
     
